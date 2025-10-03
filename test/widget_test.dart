1352862e470ae71e87546a6d346acef01b9546e7
Siranjeevan <smartjeevith2k@gmail.com>
2025-10-04 02:37:45 +0530

    google auth firebase changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nadi_pariksh/main.dart';
 
 void main() {
-  testWidgets('App launches successfully', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const NadiParikshaApp());
-    expect(find.text('Nadi Pariksh'), findsOneWidget);
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
